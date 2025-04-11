c099bd450a7bd1cc5d2cd4e0e1565a9e31934d92
Dilyara <Dilyarasavela13@gmail.com>
2025-04-11 14:01:07 +0300

    change main string

--- src/test/java/basics/lesson1/HelloWorld.java
+++ src/test/java/basics/lesson1/HelloWorld.java
@@ -86,6 +86,6 @@ public class HelloWorld {
         @Test
         public void tryMe() {
             System.out.println("Hello World");
-            System.out.println("Another String");
+            System.out.println("Another other String");
         }
     }
